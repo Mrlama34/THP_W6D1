b651354855dd2cb33078ea1655f4a7aed942e111
LeoDeb <leo.deboin@gmail.com>
2018-05-08 17:37:30 +0200

    debut event

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,37 @@
+class EventsController < ApplicationController
+	def new
+	@event = Event.new
+	end
+
+	def create
+		if user_signed_in?
+			@current_user = current_user
+			@event = Event.new("params_event")
+			@event.creator_id = @current_user.id
+			@event.save
+			if @event.save
+				redirect_to event_path(@event.id)
+			else 
+				flash.now[:danger] = "Erreur dans la création de votre evenement"
+				render new
+			end
+		else
+			redirect_to new_user_session_path
+		end
+
+
+	end
+
+	def index
+		@events = Event.all
+	end
+
+	def show
+
+	end
+private
+
+def params_event
+	params.require(:event).permit(:description, :date, :place)
+end
+end
